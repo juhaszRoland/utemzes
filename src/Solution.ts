756199c390048c27b5e1fba4d035ecd449917f10
Horvath Mark <horvath.mark1@students.jedlik.eu>
2024-11-13 11:39:54 +0100

    Created Camp class

---
+++ src/Solution.ts
@@ -0,0 +1,24 @@
+import Camp from "./Camp";
+import fs from "fs";
+
+export default class Solution {
+    #camps: Camp[] = [];
+
+    constructor(path: string) {
+        try {
+            const data = fs.readFileSync(path, "utf-8");
+            data.split("\n").forEach(row => {
+                const actRow: string = row.trim();
+                if (actRow.length > 0) {
+                    this.#camps.push(new Camp(actRow));
+                }
+            });
+        } catch (error) {
+            if (error instanceof Error) {
+                console.error(`Hiba a fájl beolvasásakor: ${error.message}`);
+            } else {
+                console.error("Ismeretlen hiba történt.");
+            }
+        }
+    }
+}
